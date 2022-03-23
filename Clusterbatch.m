3c31fc55afdba5da41c7951edb7d43d9bce123bd
HousseinAyoub <68450522+HousseinAyoub@users.noreply.github.com>
2022-03-24 01:04:29 +0300

    Delete Clusterbatch.m

--- Clusterbatch.m
+++
@@ -1,5 +0,0 @@
-clust = parcluster();
-job = createCommunicatingJob(clust,'Type','Pool');
-task = createTask(job, @Multiple_Runs_for_Average, 108, {2}); %112 is the number of ourputs and {2} is the number of estimated parameters 
-tic; submit(job); toc
- 
