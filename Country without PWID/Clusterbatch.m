94446c024cff2881c8b63457592ea195c803bb7a
HousseinAyoub <68450522+HousseinAyoub@users.noreply.github.com>
2022-03-24 02:06:50 +0300

    Delete Country without PWID directory

--- Country without PWID/Clusterbatch.m
+++
@@ -1,4 +0,0 @@
-clust = parcluster();
-job = createCommunicatingJob(clust,'Type','Pool');
-task = createTask(job, @Multiple_Runs_for_Average, 79, {1}); %112 is the number of ourputs (HIAM please check this) and {1} is the number of estimated parameter  
-tic; submit(job); toc
